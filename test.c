75e7ffb7241fc6e957371fbd23902bdef6324fd4
schema-0 <schematique@pusan.ac.kr>
2022-08-07 17:06:27 +0900

    comment added at master

--- test.c
+++ test.c
@@ -2,7 +2,6 @@
 // 이 파일은 22-08-07 15:29 로컬에서 수정되었다
 // 이 파일은 22-08-07 15:41 git에서 에디터로 수정되었다
 // file modifed in git bash before commit
-// modified at 'master' + modified at 'devel'
 using namespace std;
 
 int main( void ) {
@@ -22,3 +21,4 @@ int main( void ) {
 
 	return 0;
 }
+// added comment at master
